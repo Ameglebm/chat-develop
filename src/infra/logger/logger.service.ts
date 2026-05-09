6c35e6408d6306cd7786ee990351b68d58e21017
Ameglebm <ameglevr@gmail.com>
2026-05-09 03:29:59 -0300

    Feat - Fix docker e logger

--- src/infra/logger/logger.service.ts
+++ src/infra/logger/logger.service.ts
@@ -26,9 +26,7 @@ const DEFAULT_THEME: LoggerTheme = {
   color: LOGGER_COLORS.gray,
 };
 
-@Injectable({
-  scope: Scope.TRANSIENT,
-})
+@Injectable()
 export class LoggerService implements NestLoggerService {
   private context?: string;
 
